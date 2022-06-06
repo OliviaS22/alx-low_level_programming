43267b5c9c298cfbb6abb9325121fa781b7fe8e5
Olivia Swarts <swartsolivia@gmail.com>
2022-06-06 08:35:13 -0700

    printcomb

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,27 +5,31 @@
 /**
  * main - Entry point
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int p, q;
+	int d, p, q;
 
-	for (p = 0; p <= 98; p++)
+	for (d = '0'; d < '9'; d++)
 	{
-		for (q = p + 1; q <= 99; q++)
+		for (p = d + 1; p <= '9'; p++)
 		{
-			putchar((p / 10) + '0');
-			putchar((p % 10) + '0');
-			putchar(' ');
-			putchar((q / 10) + '0');
-			putchar((q % 10) + '0');
+			for (q = p + 1; q <= '9'; q++)
+			{
+				if ((p != d) != q)
+				{
+					putchar(d);
+					putchar(p);
+					putchar(q);
 
-			if (p == 98 && q == 99)
-				continue;
-			
-			putchar(',');
-			putchar(' ');
+					if (d == '7' && p == '8')
+						continue;
+					
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
 	putchar('\n');
