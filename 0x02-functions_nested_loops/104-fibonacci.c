d061e719e532eb8482b712c8e6428840a26fc565
Olivia Swarts <swartsolivia@gmail.com>
2022-06-08 15:02:24 -0700

    final

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,13 +12,10 @@
  */
 int main(void)
 {
-
 	unsigned long int i, j, k, j1, j2, k1, k2;
 
 	j = 1;
-
 	k = 2;
-
 	printf("%lu", j);
 
 	for (i = 1; i < 91; i++)
@@ -26,19 +23,13 @@ int main(void)
 	{
 
 		printf(", %lu", k);
-
 		k = k + j;
-
 		j = k - j;
-
 	}
 
 	j1 = j / 1000000000;
-
 	j2 = j % 1000000000;
-
 	k1 = k / 1000000000;
-
 	k2 = k % 1000000000;
 
 	for (i = 92; i < 99; ++i)
@@ -52,17 +43,9 @@ int main(void)
 		k1 = k1 + j1;
 
 		j1 = k1 - j1;
-
 		k2 = k2 + j2;
-
 		j2 = k2 - j2;
-
 	}
-
-	printf("\n");
-
-	return (0);
-
+printf("\n");
+return (0);
 }
-
-
