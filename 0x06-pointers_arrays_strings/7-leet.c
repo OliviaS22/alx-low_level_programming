90db7819327577f3d3509366074445fa23f65e23
Olivia Swarts <swartsolivia@gmail.com>
2022-06-16 04:42:12 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,39 +8,30 @@
 
 char *leet(char *s)
 {
-
 	int i;
 
 	int j;
 
 	int k;
 
-
 	char l[] = "oOlLeEaAtT";
 
 	char e[] = "0011334477";
 
-
 	i = 0;
 
 	while (s[i] != '\0')
-
 	{
 
 		j = 0;
-
 		k = 0;
 
 		while (l[j] != '\0')
-
 		{
 
 			if (s[i] == l[j])
-
 			{
-
 				k = j;
-
 				s[i] = e[k];
 
 			}
@@ -50,9 +41,7 @@ char *leet(char *s)
 		}
 
 		i++;
-
 	}
 
 	return (s);
-
 }
