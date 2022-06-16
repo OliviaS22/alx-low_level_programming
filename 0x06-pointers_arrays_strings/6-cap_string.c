c5c0859c0ee46e082b7389fb4cda6e3ed793f695
Olivia Swarts <swartsolivia@gmail.com>
2022-06-16 04:33:26 -0700

    6cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,19 +5,13 @@
  */
 
 char *cap_string(char *s)
-
 {
 
 	int i;
 
-
-
 	i = 0;
-
 	while (s[i] != '\0')
-
 	{
-
 		if ((s[i - 1] == ' ' || s[i - 1] == '\n'
 
 					|| s[i - 1] == '\t' || s[i - 1] == ','
@@ -33,33 +27,22 @@ char *cap_string(char *s)
 					|| s[i - 1] == '.')
 
 				&& (s[i] >= 'a' && s[i] <= 'z'))
-
 		{
-
 			s[i] = s[i] - 32;
-
 		}
 
 		else if ((s[0] >= 97 && s[0] <= 122))
 
 		{
-
 			s[0] = s[0] - 32;
-
 		}
-
 		else
 
 		{
-
 			s[i] = s[i];
-
 		}
 
 		i++;
-
 	}
-
 	return (s);
-
 }
