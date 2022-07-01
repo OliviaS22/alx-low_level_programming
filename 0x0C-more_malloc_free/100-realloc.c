8269dda15a8f41df30563d55d1e157de3f8d74c9
Olivia <swartsolivia@gmail.com>
2022-07-01 08:10:48 -0700

    q5

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,29 +17,18 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i;
 
 	if (new_size == old_size)
-
 		return (ptr);
 
 	if ((new_size == 0) && (ptr != NULL))
-
 	{
-
 		free(ptr);
-
 		return (NULL);
-
 	}
-
 	if (ptr == NULL)
-
 	{
-
 		nptr = malloc(new_size);
-
 		if (nptr == NULL)
-
 			return (NULL);
-
 	}
 	if (new_size > old_size && (ptr != NULL))
 	{
@@ -52,5 +41,4 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 	}
 	return (nptr);
-
 }
